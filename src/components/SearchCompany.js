53f35981999ff729e3b00c6eb97bd454e77de2fc
narisasingngam <mmintttt@gmail.com>
2019-12-02 13:14:25 +0700

    complete project

--- src/components/SearchCompany.js
+++ src/components/SearchCompany.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios';
 import { CompanyTable } from './CompanyTable'
 import './../styles/search_company.css'
+import Card from 'react-bootstrap/Card';
 
 export class SearchCompany extends Component {
 
@@ -11,7 +12,7 @@ export class SearchCompany extends Component {
             companyInsuranceDetail: [],
             companyList: [],
             searchCompany: [],
-            onClickName:""
+            onClickName: ""
         }
         this.callCompanyAPI()
     }
@@ -26,7 +27,7 @@ export class SearchCompany extends Component {
     }
 
     handleCompanyInput = (event) => {
-        this.setState({onClickName: event.target.value})
+        this.setState({ onClickName: event.target.value })
         const filterValues = (name) => {
             return this.state.companyList.filter(data => {
                 return data.company_name.toLowerCase().indexOf(name.toLowerCase()) > -1;
@@ -42,8 +43,8 @@ export class SearchCompany extends Component {
 
     clickCompany(companyValue) {
         console.log(companyValue)
-        this.setState({searchCompany:[],onClickName: companyValue})
-        axios.post('https://insuranceapii.herokuapp.com/company/search', {company : companyValue})
+        this.setState({ searchCompany: [], onClickName: companyValue })
+        axios.post('https://insuranceapii.herokuapp.com/company/search', { company: companyValue })
             .then(res => {
                 this.setState({ companyInsuranceDetail: res.data })
             })
@@ -53,26 +54,54 @@ export class SearchCompany extends Component {
         const items = this.state.searchCompany.map((item, key) =>
             <button className="company-search" key={item.id} onClick={() => this.clickCompany(item.company_name)}>{item.company_name}</button>
         )
+        const companyButton = this.state.companyInsuranceDetail.map((item, key) =>
+            <div className="card-company">
+                <Card style={{ width: '18rem'}}>
+                    <Card.Body>
+                        <Card.Title>{item.company_name}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">{item.program_name}</Card.Subtitle>
+                        <Card.Text>
+                            <center>
+                            Premium Rate(per year)
+                            </center>
+                            <center>
+                            <h3>฿{item.premium_rate}</h3>
+                            </center>
+                        </Card.Text>
+                        <Card.Text>
+                            Maximum Coverage: {item.covered_expense}
+                        </Card.Text>
+                        <Card.Text>
+                            Coverage: {item.category}
+                        </Card.Text>
+                        <Card.Text>
+                            Policy Period: {item.policy_period}
+                        </Card.Text>
+                        
+                    
+                    </Card.Body>
+                </Card>
+            </div>
+        )
         return (
             <div className="container">
                 <div className="search-bar">
-                    <input 
-                    className="company-input" 
-                    placeholder=" type company name here"
-                    onChange={this.handleCompanyInput}
-                    value={this.state.onClickName}
+                    <input
+                        className="company-input"
+                        placeholder=" type company name here"
+                        onChange={this.handleCompanyInput}
+                        value={this.state.onClickName}
                     />
                     <div className="scroll-company">
                         {items}
                     </div>
                     <div>
-                    <div className="table-company">
-                    <div className="text-center">Company Insurance Detail</div>
-                    <CompanyTable companyInsuranceDetail={this.state.companyInsuranceDetail} />
+                        <div className="table-company">
+                            {companyButton}
+                        </div>
                     </div>
                 </div>
-                </div>
-                
+
             </div>
         )
     }
